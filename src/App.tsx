c0803651baf3f09a13ffaa784686e015bc0c1cc9
muchafilip <filipmucha94@gmail.com>
2023-11-24 23:09:01 +0100

    removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-// import AddWorkout from './components/AddWorkout';
-// import AddWorkoutNew from './components/AddWorkoutV2';
-
 import ExerciseSuggestionComponent from './components/GenerateExercises';
-import OneSetViewer from './components/GetFirstSet';
-import WorkoutDay from './components/WorkoutPlans/WorkoutDay';
-import WorkoutPlanViewer from './components/WorkoutPlans/WorkoutPlanViewer';
 import WorkoutPlanPage from './components/WorkoutPlans/screens/WorkoutPlanPage';
-
-
-
 
 const App: React.FC = () => {
 
@@ -25,17 +16,12 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/">Home</Link>| <Link to="/add-workout-new">Add WorkoutV2</Link> | <Link to="/add-workout">Add Workout</Link> | <Link to="/workout-plans">Workout Plans</Link> | <Link to="/generate-exercises">Generate Exercises</Link> | <Link to="/test-wd-component">WD Component</Link> | <Link to="/one-set-viewer">One Set Viewer</Link> | <Link to="/workout-plan">workout plan Viewer</Link>| <Link to="/workout">workout</Link>
+        <Link to="/">Home</Link> | <Link to="/generate-exercises">Generate Exercises</Link> | <Link to="/workout">workout</Link>
       </nav>
       <Routes>
         <Route path="/" element={<h1>Welcome to the Fitness Tracker</h1>} />
-        {/* <Route path="/add-workout" element={<AddWorkout />} /> */}
         <Route path="/generate-exercises" element={<ExerciseSuggestionComponent />} />
-        {/* <Route path="/test-wd-component" element={<WorkoutDayComponent workoutDay={workoutDay} />} /> */}
-        <Route path="/one-set-viewer" element={<OneSetViewer />} />
-        {/* <Route path="/exercises-viewer" element={<WorkoutDay />} /> */}
-        <Route path="/workout-plan" element={<WorkoutPlanViewer />} />
-        {/* <Route path="/add-workout-new" element={<AddWorkoutNew />} /> */}
+
         <Route path="/workout" element={<WorkoutPlanPage />} />
 
       </Routes>
